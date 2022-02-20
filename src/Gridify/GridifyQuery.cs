24dce0904c82b3f7157096bb9abd794582d7d53f
eation <allan_eation@hotmail.com>
2022-02-20 16:02:37 +0800

    feat: update

--- src/Gridify/GridifyQuery.cs
+++ src/Gridify/GridifyQuery.cs
@@ -5,18 +5,20 @@ public class GridifyQuery : IGridifyQuery
    public GridifyQuery()
    {
    }
-   public GridifyQuery(int page, int pageSize, string filter, string? orderBy = null, string? select = null)
+   public GridifyQuery(int page, int pageSize, string? filter = null, string? orderBy = null, string? select = null)
    {
+      Filter = filter;
+      OrderBy = orderBy;
+      Select = select;
+
       Page = page;
       PageSize = pageSize;
-      OrderBy = orderBy;
-      Filter = filter;
-      Select = select;
    }
+
+   public string? Filter { get; set; }
+   public string? OrderBy { get; set; }
+   public string? Select { get; set; }
 
    public int Page { get; set; }
    public int PageSize { get; set; }
-   public string? OrderBy { get; set; }
-   public string? Filter { get; set; }
-   public string? Select { get; set; }
 }
